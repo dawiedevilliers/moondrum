11f792c58bf7dad0b756b356454fba97a35395b3
Dawie de Villiers <dawiedevilliers1@gmail.com>
2019-08-18 22:39:02 +0200

    Paralax rebuild of home screen

--- src/app/navigation/pages/home/home.component.ts
+++ src/app/navigation/pages/home/home.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import * as Parallax from 'parallax-js';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('mainscene') parallaxScene: ElementRef;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    this.setupParallax();
+  }
+
+  setupParallax() {
+    const scene = this.parallaxScene.nativeElement;
+
+    if (scene !== null) {
+      const parallaxInstance = new Parallax(scene, {
+        relativeInput: true,
+        hoverOnly: true
+      });
+    }
+
   }
 
 }
