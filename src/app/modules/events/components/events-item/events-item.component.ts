30af2bc0a9c66f3a2ff9b08890a8888db0d86b3b
Dawie de Villiers <dawiedevilliers1@gmail.com>
2019-08-21 06:36:24 +0200

    Looking gewd - only need to do gallery instantiation

--- src/app/modules/events/components/events-item/events-item.component.ts
+++ src/app/modules/events/components/events-item/events-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { MoonDrumEvent } from '../../../../classes/MoonDrumEvent';
 
 @Component({
   selector: 'app-events-item',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./events-item.component.scss']
 })
 export class EventsItemComponent implements OnInit {
+
+
+  @Input() event: MoonDrumEvent = null;
 
   constructor() { }
 
--- src/app/modules/events/components/events-selection/events-selection.component.ts
+++ src/app/modules/events/components/events-selection/events-selection.component.ts
@@ -1,15 +1,63 @@
-import { Component, OnInit } from '@angular/core';
+import { MoonDrumEvent } from './../../../../classes/MoonDrumEvent';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { GalleryImage } from '../../../../classes/gallery-image';
 
 @Component({
   selector: 'app-events-selection',
   templateUrl: './events-selection.component.html',
   styleUrls: ['./events-selection.component.scss']
 })
-export class EventsSelectionComponent implements OnInit {
+export class EventsSelectionComponent implements OnInit, AfterViewInit {
+
+
+
+  events: MoonDrumEvent[] = new Array();
 
   constructor() { }
 
   ngOnInit() {
+    this.setupEvents();
+  }
+
+  ngAfterViewInit(): void {
+   
+  }
+
+  setupEvents() {
+
+    this.setupDayOutOfTime();
+    this.setupDreamCode();
+    this.setupEarthDance();
+
+  }
+
+  setupDayOutOfTime() {
+    let eventImages = this.retrieveImages('/assets/Images/Events/DayOutofTime/', 42);
+    this.events.push(new MoonDrumEvent('Day Out Of Time', '/assets/Images/Events/DayOutofTime/11.JPG', eventImages));
+  }
+
+  setupDreamCode() {
+    let eventImages = this.retrieveImages('/assets/Images/Events/Dreamcode/', 2);
+    this.events.push(new MoonDrumEvent('Dream Code', '/assets/Images/Events/Dreamcode/1.JPG', eventImages));
+  }
+
+  setupEarthDance() {
+    let eventImages = this.retrieveImages('/assets/Images/Events/Earthdance2017/', 26, '.jpg');
+    this.events.push(new MoonDrumEvent('Earth dance 2017', '/assets/Images/Events/Earthdance2017/1.jpg', eventImages));
+  }
+
+  retrieveImages(baseUrl: string, count: number, extension: string = '.JPG'): GalleryImage[] {
+
+    var returnImage: GalleryImage[] = new Array();
+
+    let i = 1;
+
+    for (0; i < count; i++) {
+      returnImage.push(new GalleryImage('test', baseUrl + i + extension));
+    }
+
+    return returnImage;
+
   }
 
 }
